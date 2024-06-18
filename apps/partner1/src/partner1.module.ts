f0dff5c9b3d69d8e91a4af84b0eaaf18d0b6400f
Luiz Augusto <augustodia97@gmail.com>
2024-06-17 21:04:30 -0300

    ✨ add auth to controller

--- apps/partner1/src/partner1.module.ts
+++ apps/partner1/src/partner1.module.ts
@@ -3,13 +3,16 @@ import { EventsModule } from './events/events.module';
 import { SpotsModule } from './spots/spots.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '../../../libs/core/src/prisma/prisma.module';
+import { AuthModule } from '@app/core/auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env.partner1',
+      isGlobal: true,
     }),
     PrismaModule,
+    AuthModule,
     EventsModule,
     SpotsModule,
   ],
